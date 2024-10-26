c27deb2399a44275919fd7cf5a707fb8a486e14c
nguyenvan-trung <nguyenvantrungtn4510273@gmail.com>
2024-10-26 13:46:00 +0700

    Add files via upload

---
+++ he.py
@@ -0,0 +1,14 @@
+print("nhapa: ");
+a =  int(input());
+b =  int(input());
+print("a + b =", a + b);
+print("a - b =", a - b);
+print("a * b =", a * b);
+print("a // b =", a // b);
+print("a ** b =", a ** b);
+print("a > b =", a > b);
+print("a & b =", a & b);
+print("a | b =", a | b);
+print("a ^ b =", a ^ b);
+print("~ a =", ~a);
+print("(a >> 5) =", a>>5);
