a3d4230803a5d0a9ee02708cfc1bae44a590ab1b
nguyenvan-trung <nguyenvantrungtn4510273@gmail.com>
2024-11-17 20:54:55 +0700

    DEADLINE: HẾT NGÀY 21/11/2024

--- bai3.py
+++ bai3.py
@@ -1,17 +1,5 @@
-n =  int(input("nhap n: "))
-m = int(input("nhap m: "))
-
-nums1 =  [input("P: ") for _ in range(n)]
-nums2 =  [input("S: ") for _ in range(m)]
-def tim(a,b):
-    lst =  []
-    for i in a[::-1]:
-        for j in b[::-1]:
-            if i == j:
-                lst.append(i)
-                a.remove(i)
-                b.remove(j)
-                break
-    return lst
-
-print(f'ham la:{tim(nums1, nums2)}')
+country = {
+    'trung':'vietnam';
+    'dao':'bacninh';
+    'hieu':'landon'
+}
