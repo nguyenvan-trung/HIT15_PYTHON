7b1b4ae74ff4bfe5dde92f047e2396225ecbcc3e
nguyenvan-trung <nguyenvantrungtn4510273@gmail.com>
2024-11-22 19:23:18 +0700

    BAITEXT

--- problem3.py
+++ problem3.py
@@ -1,17 +1,17 @@
 cod = input("nhap chuoi: ")
 a  = ""
-k = len(cod)
+
 h = 0
 b = ""
 c = ""
 for i in cod:
     if i <= '9' and i >= '1':
-        h =  i
+        h =  int(i)
     if i != '[' and i != ']' and i > '9' and i < '1':
         a+=i
     if i == '[' : b = '['
     if i == ']' : b = ']'
-    if b == '[' : c +=i
+    if b == '[' and i != '[': c +=i
     if b == ']':
         for j in range(h-1): a+=c
         c = ""
